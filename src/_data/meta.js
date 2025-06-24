6506fdd731701a179aee7ef35a75d068743c21f2
Jeremy Pogue <jpogue@usc.edu>
2025-06-23 21:40:24 -0400

    updated poster

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -24,7 +24,7 @@ export const pathToSvgLogo = 'src/assets/svg/misc/logo-light.svg'; // used for f
 export const themeColor = '#233a52'; // used in manifest, for example primary color value
 export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light colors are prefered. best use value set for light bg
 export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
-export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
+export const opengraph_default = '/assets/images/template/opengraph-default.png'; // fallback/default meta image
 export const opengraph_default_alt =
   "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
 export const blog = {
---
+++ src/pages/scavenger.md
@@ -0,0 +1,8 @@
+---
+title: The SCavenger
+permalink: /highlights/scavenger/index.html
+description: An interactive adventure journal embedded system that Jeremy Pogue developed for his senior capstone project.
+layout: page
+---
+
+test
